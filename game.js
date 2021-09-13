4e727d96818f8333fd5e9ec2aa16a5a7c4f3d885
Sean Thomo <seansthomo@gmail.com>
2021-09-13 17:57:44 +0200

    added button and score

--- game.js
+++ game.js
@@ -4,12 +4,16 @@ let userClickedPattern = [];
 let started = false;
 let level = 0;
 
-$(document).keypress(function() { 
+$('.start-btn').click(function() { 
   if(!started){
-    $('#level-title').text(`Level ${level}`)
-    nextSequence();
-    started = true;
+    setTimeout(function(){
+      $('#level-title').text(`Level ${level}`)
+      nextSequence();
+      started = true;
+    },500)
   }
+  $('.start-btn').hide();
+
 });
 
 $('.btn').click(function() {
@@ -60,14 +64,18 @@ function checkAnswer(currentLevel){
       $('body').removeClass('game-over');
     }, 200)
 
-    $('#level-title').text(`Game Over, Press Any Key to Restart`);
-
-    startOver()
+    $('#level-title').text(`Game Over, High Score: ${level}.`);
+    startOver();
   }
 }
 
 function startOver(){
-  level = 0;
-  gamePattern = [];
-  started = false;
+  setTimeout(function(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+    $('.start-btn').show();
+    $('.start-btn').text('Restart');
+  },500)
+
 }
